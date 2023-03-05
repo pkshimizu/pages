75711ed7206b5208b5ae1cca742c1c1f37e676c0
pkshimizu <pkshimizu@users.noreply.github.com>
2023-03-05 06:50:35 +0000

    deploy: 51a375f15581f47a7b5de484545ddb8b0cf4aa29

--- _next/static/EBnMF4uaPG5bAY2RtoUEN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/43225ca8d659a19d.css","static/chunks/pages/index-0b625b84d15f1e7f.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
